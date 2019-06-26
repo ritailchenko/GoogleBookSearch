49563785bb3c0dd32ab70656b61d1cd82184953f
Rita <rita@dyn-129-236-238-182.dyn.columbia.edu>
2019-06-26 17:26:52 -0400

    changed logo

--- client/src/components/Logo/index.js
+++ client/src/components/Logo/index.js
@@ -4,7 +4,8 @@ import "./style.css"
 function Logo() {
     return (
             <div className="logo">
-                <img className="logo-position" src="http://icons.iconarchive.com/icons/dtafalonso/android-lollipop/256/Play-Books-icon.png" />
+                {/* <img className="logo-position" src="http://icons.iconarchive.com/icons/dtafalonso/android-lollipop/256/Play-Books-icon.png" /> */}
+                <img className="logo-position" src="https://i.pinimg.com/564x/2b/7a/c7/2b7ac798cd239a09765c486f71d2f931.jpg" />
                 <h1 className="logo-text logo-position"> (React) Google Books Search </h1>
             </div>
     )
